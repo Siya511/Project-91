7e7d6a61799a3c5d4f62ee59c7c4ea9552a507d0
Siya511 <84263658+Siya511@users.noreply.github.com>
2021-05-20 15:10:52 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function addUser()
 {
-    number1_name = document.getElementById("number1_name_input").value;
-    number2_name = document.getElementById("number2_name_input").value;
+    number1_name = document.getElementById("player1_name_input").value;
+    number2_name = document.getElementById("player2_name_input").value;
 
     localStorage.setItem("number1",number1_name);
     localStorage.setItem("number2",number2_name);
